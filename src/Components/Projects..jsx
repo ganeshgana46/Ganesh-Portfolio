beba1a37b2ca838577b10b10a36de68285ae2fea
pallamganesh <pallamganesh213@gmail.com>
2024-04-08 23:57:37 +0530

    third commit

--- src/Components/Projects..jsx
+++ src/Components/Projects..jsx
@@ -29,7 +29,7 @@ const Projects = () => {
         id: 3,
         src: crud,
         desc: "A predictable state container to perform Create, Read, Update, and Delete operations on data.",
-        demo: "https://magnificent-pothos-32a169.netlify.app",
+        demo: "https://luminous-bublanina-829e44.netlify.app",
         code: "https://github.com/ganeshgana46/shop-easy"
       },
   ]
